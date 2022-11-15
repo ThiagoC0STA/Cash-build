fe09307afe3e24deb6afb46e4a3f8eeb1859f88a
Thiago Rodrigues da Costa <82855801+ThiagoC0STA@users.noreply.github.com>
2022-11-15 12:00:58 -0300

    contact page

--- components/ContactForm/ContactForm.tsx
+++ components/ContactForm/ContactForm.tsx
@@ -1,5 +1,8 @@
+import Image from "next/image";
+import Link from "next/link";
 import React, { useState } from "react";
-import { FormComponent } from "./style";
+import { Facebook, Instagram, Linkedin } from "../E__export";
+import { FormComponent, MainDiv } from "./style";
 
 const ContactForm = () => {
   const [submited, setSubmited] = useState<boolean>(false);
@@ -10,14 +13,49 @@ const ContactForm = () => {
   };
 
   return (
-    <FormComponent onSubmit={handleSubmit}>
-      <input type="text" placeholder="Nome" required={true} />
-      <input type="text" placeholder="Sobrenome" />
-      <input type="email" required={true} placeholder="E-mail" />
-      <input type="tel" maxLength={15} required={true} placeholder="Telefone" />
-      <input type="text" required={true} placeholder="Digite sua mensagem" />
-      <button>Enviar</button>
-      {submited === true && <p>Obrigado pelo contato!</p>}
+    <FormComponent>
+      <MainDiv>
+        <div>
+          <h2>Nos envie uma mensagem</h2>
+
+          <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Nome" required={true} />
+            <input type="text" placeholder="Sobrenome" />
+            <input type="email" required={true} placeholder="E-mail" />
+            <input
+              type="tel"
+              maxLength={15}
+              required={true}
+              placeholder="Telefone"
+            />
+            <input
+              type="text"
+              required={true}
+              placeholder="Digite sua mensagem"
+            />
+            <button>Enviar</button>
+            {submited === true && <p>Obrigado pelo contato!</p>}
+          </form>
+        </div>
+
+        <p>Ou</p>
+
+        <div>
+          <h2>Nos chame em nossas redes</h2>
+
+          <nav>
+            <Link href="/">
+              <Image src={Facebook} alt="Facebook" width="30" height="30"/>
+            </Link>
+            <Link href="/">
+              <Image src={Instagram} alt="Instagram" width="30" height="30" />
+            </Link>
+            <Link href="/">
+              <Image src={Linkedin} alt="Linkedin" width="30" height="30" />
+            </Link>
+          </nav>
+        </div>
+      </MainDiv>
     </FormComponent>
   );
 };
--- components/ContactForm/style.tsx
+++ components/ContactForm/style.tsx
@@ -1,5 +1,116 @@
 import styled from "styled-components";
 
-export const FormComponent = styled.form`
+export const FormComponent = styled.section`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 100px 0 141px 0;
+`;
 
-`
+export const MainDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  padding: 0 4vw;
+
+  div:first-child {
+    background-color: #3b4c7e;
+    width: 40vw;
+    border-radius: 20px;
+    height: 70vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    gap: 60px;
+  }
+
+  h2 {
+    color: #fff;
+  }
+
+  div:last-child {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 100px;
+    background-color: #7c8fc7;
+    width: 40vw;
+    border-radius: 20px;
+    height: 70vh;
+
+    nav {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap: 30px;
+    }
+
+    a {
+      width: 58px;
+      height: 58px;
+      background: #fff;
+      border-radius: 4px;
+      border: none;
+      transition: all ease 0.4s;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      :hover {
+        background: #e7e9f4;
+      }
+    }
+  }
+
+  form {
+    width: 35vw;
+    height: 32vh;
+    gap: 15px;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+
+    input:nth-child(5) {
+      width: 31vw;
+    }
+
+    button {
+      padding: 10px 50px;
+      color: #fff;
+      background-color: #4970e6ca;
+      border: none;
+      border-radius: 10px;
+      font-size: 15px;
+      transition: all ease 0.4s;
+
+      :hover {
+        background-color: #929bff;
+      }
+    }
+  }
+
+  input {
+    width: 15vw;
+    height: 5vh;
+    font-size: 15px;
+    border-radius: 10px;
+    border: none;
+    background-color: #edeff6;
+    color: #000000;
+    padding-left: 15px;
+
+    ::placeholder {
+      color: #000000;
+    }
+
+    :focus {
+      outline: none !important;
+      border: none;
+    }
+  }
+`;
