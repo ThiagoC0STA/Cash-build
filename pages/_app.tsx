e4d70c70505c48bc47ba4d87e8ea9b6bc5424c4e
Thiago Rodrigues da Costa <82855801+ThiagoC0STA@users.noreply.github.com>
2023-02-01 17:52:49 -0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Cash Building</title>
-        <meta name="description" content="A melhor imobiliária da região" />
+        <meta name="description" content="tenha a experiência de comprar ou alugar o imóvel perfeito. Com uma equipe altamente capacitada e uma ampla gama de opções." />
       </Head>
 
       <GlobalContext>
